959470bc03e68d3c0077d920bdcf28562b686ff0
Lokesh Mohanty <me.lokeshmohanty@gmail.com>
2023-11-30 12:26:37 +0530

    add syncthing

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -35,6 +35,7 @@ in
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
+    ./syncthing.nix
   ];
 
   nixpkgs = {
---
+++ nixos/syncthing.nix
@@ -0,0 +1,45 @@
+{ ... }:
+
+{
+  services.syncthing = {
+    enable = true;
+    user = "lokesh";
+    dataDir = "/home/lokesh/.local/share/syncthing";
+    configDir = "/home/lokesh/.config/syncthing";
+    openDefaultPorts = true;
+    overrideDevices = true;
+    overrideFolders = true;
+    settings = {
+      devices = {
+        "lab" = { id = "BKTCSPE-QCLARTR-5UVN7OB-NRHFAJI-SG36GWR-6NDDDO7-FLQVPV6-MGIU6AT"; }; 
+        "phone" = { id = "KFX44SL-S6S375R-BJGJWI7-PM74ONC-MNYVYF4-YNDSESJ-EQWEWKE-JZHHJQN"; };
+      };
+      folders = {
+        "Projects" = {
+          path = "/home/lokesh/Projects";
+          devices = [ "lab" ];
+        };
+        "Research" = {
+          path = "/home/lokesh/Documents/Research";
+          devices = [ "lab" ];
+        };
+        "Presentations" = {
+          path = "/home/lokesh/Documents/Presentations";
+          devices = [ "lab" ];
+        };
+        "Books" = {
+          path = "/home/lokesh/Documents/Books";
+          devices = [ "lab" ];
+        };
+        "Practice" = {
+          path = "/home/lokesh/Desktop/Practice";
+          devices = [ "lab" ];
+        };
+        "Org" = {
+          path = "/home/lokesh/Documents/Org";
+          devices = [ "phone" ];
+        };
+      };
+    };
+  };
+}
