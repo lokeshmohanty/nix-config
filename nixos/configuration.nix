ed9772f86cd3b859afb6a3b6443ae7ff587febda
Lokesh Mohanty <me.lokeshmohanty@gmail.com>
2024-02-10 17:11:44 +0530

    update cachix settings

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -63,8 +63,14 @@
       trusted-users = [ "lokesh" ];
 
       # cachix
-      # substituters = [ "https://nix-gaming.cachix.org" ];
-      # trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
+      substituters = [
+        "https://nix-community.cachix.org"
+        # "https://nix-gaming.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        # "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      ];
     };
     gc = {
       automatic = true;
