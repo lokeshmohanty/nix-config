c7937b927d065c1d6bc96d0cee15569b9205edb7
Lokesh Mohanty <me.lokeshmohanty@gmail.com>
2024-02-11 20:47:04 +0530

    use sddm instead of greetd

--- nixos/greeter.nix
+++ nixos/greeter.nix
@@ -1,24 +1,23 @@
 { pkgs, ... }: {
 
-  # Enable the X11 windowing system.
-  # services.xserver = {
-  #   enable = true;
-
-  #   displayManager = {
-  #     sddm.enable = true;
-  #     sddm.wayland.enable = true;
-  #     # sddm.theme = "breeze";
-  #     # defaultSession = "Hyprland";
-  #   };
-  #   desktopManager.plasma5.enable = true;
-  # };
-  services.greetd = {
+  environment.systemPackages = with pkgs; [ where-is-my-sddm-theme ];
+  services.xserver = {
     enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.hyprland}/bin/Hyprland";
-        user = "lokesh";
-      };
+    displayManager = {
+      sddm.enable = true;
+      # sddm.theme = "${import ./sddm-theme.nix { inherit pkgs; }}";
+      sddm.theme = "where_is_my_sddm_theme";
+      defaultSession = "hyprland";
     };
+    desktopManager.mate.enable = true;
   };
+  # services.greetd = {
+  #   enable = true;
+  #   settings = {
+  #     default_session = {
+  #       command = "${pkgs.hyprland}/bin/Hyprland";
+  #       user = "lokesh";
+  #     };
+  #   };
+  # };
 }
