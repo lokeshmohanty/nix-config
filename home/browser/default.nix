5b9575e7007602496612237b170752c5f2315a06
Lokesh Mohanty <me.lokeshmohanty@gmail.com>
2025-03-29 21:11:02 +0530

    browser: configure qutebrowser

--- home/browser/default.nix
+++ home/browser/default.nix
@@ -10,6 +10,20 @@
       ud = "https://www.urbandictionary.com/define.php?term={}";
       gr = "https://grok.com/?q={}";
     };
+    settings = {
+      auto_save.session = true;
+      url.default_page = "qute://bookmarks";
+      url.start_pages = ["qute://bookmarks"];
+      content.autoplay = false;
+    };
+    keyBindings = {
+      normal = {
+        "H" = "tab-prev";
+        "L" = "tab-next";
+        "J" = "back";    # back in history
+        "K" = "forward"; # forward in history
+      };
+    };
   };
 }
 
