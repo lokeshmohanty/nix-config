bcb1e7c42e0c72c7efd8055bf0a4939b1dfc493b
Lokesh Mohanty <me.lokeshmohanty@gmail.com>
2025-04-26 21:28:13 +0530

    neovim: update terminal keymap

--- modules/neovim/lua/myConfig/terminal.lua
+++ modules/neovim/lua/myConfig/terminal.lua
@@ -14,9 +14,14 @@ vim.keymap.set("n", "<leader>tt", ":FloatermToggle<cr>", { desc = "toggle term" 
 vim.keymap.set("t", "<leader>tt", "<C-\\><C-n>:FloatermToggle<cr>", { desc = "toggle term" })
 vim.keymap.set("n", "<leader>tk", ":FloatermKill<cr>", { desc = "kill term" })
 vim.keymap.set("t", "<leader>tk", "<C-\\><C-n>:FloatermKill<cr>", { desc = "kill term" })
-vim.keymap.set("v", "<leader>ts", "y<C-w>w<C-\\><C-n>p<cr><C-\\><C-n><C-w>w", { desc = "send selection to term" })
+vim.keymap.set(
+	"v",
+	"<leader>ts",
+	"y<C-w><C-w><C-\\><C-n>p<cr><C-\\><C-n><C-w><C-w>",
+	{ desc = "send selection to term" }
+)
 vim.keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h")
 vim.keymap.set("t", "<C-w>j", "<C-\\><C-n><C-w>j")
 vim.keymap.set("t", "<C-w>k", "<C-\\><C-n><C-w>k")
 vim.keymap.set("t", "<C-w>l", "<C-\\><C-n><C-w>l")
-vim.keymap.set("t", "<C-w>w", "<C-\\><C-n><C-w>w")
+vim.keymap.set("t", "<C-w><C-w>", "<C-\\><C-n><C-w><C-w>")
