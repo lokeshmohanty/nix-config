6f5c80f42da610b9f272c228da8847ea429fa4b2
Lokesh Mohanty <lokesh1197@gmail.com>
2025-08-08 10:24:08 +0530

    emacs: add doom-everforest-theme

--- home/config/doom/config.el
+++ home/config/doom/config.el
@@ -34,7 +34,17 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;; (setq doom-theme 'doom-one)
+
+;; `gruvbox-material' contrast and palette options
+(setq doom-everforest-background  "medium")  ; or hard (defaults to soft)
+
+;; ;; `gruvbox-material-light' contrast and palette options
+;; (setq doom-everforest-light-background  "medium") ; or hard (defaults to soft)
+
+;; set `doom-theme'
+(setq doom-theme 'doom-everforest) ; dark variant
+;; (setq doom-theme 'doom-everforest-light) ; light variant
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
