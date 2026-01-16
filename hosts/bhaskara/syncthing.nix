6ecb8dcbcac1f7b8cf07e12742a90f74d6303f63
Lokesh Mohanty <lokesh1197@gmail.com>
2026-01-16 12:53:47 +0530

    ignore files in syncthing

--- hosts/bhaskara/syncthing.nix
+++ hosts/bhaskara/syncthing.nix
@@ -2,48 +2,55 @@
   services.syncthing = {
     enable = true;
     user = "lokesh";
-    dataDir = "/home/lokesh/.local/share/syncthing";
-    configDir = "/home/lokesh/.config/syncthing";
     openDefaultPorts = true;
-    overrideDevices = true;
-    overrideFolders = true;
+    # overrideDevices = true;
+    # overrideFolders = true;
     settings = {
       devices = {
-        "laptop".id = "ZUAVAEV-LA24HLW-EIXNXCB-O7DEVOB-CJWPE22-SH5JKJY-MYCOLMN-EUK5LAS";
-        "phone" = {id = "KITNSPR-KMBOSUP-RLJS4XX-KILL76P-SR3HAEL-VKQR4U3-JN636OM-FXNZ3AX";};
+        "laptop" = {
+          id = "ZUAVAEV-LA24HLW-EIXNXCB-O7DEVOB-CJWPE22-SH5JKJY-MYCOLMN-EUK5LAS";
+          autoAcceptFolders = true;
+        };
+        "phone" = {
+          id = "KITNSPR-KMBOSUP-RLJS4XX-KILL76P-SR3HAEL-VKQR4U3-JN636OM-FXNZ3AX";
+          autoAcceptFolders = true;
+        };
       };
       folders = {
         "Projects" = {
           path = "/home/lokesh/Projects";
           devices = ["laptop"];
+          ignorePatterns = [".venv/*"];
         };
         "Research" = {
           path = "/home/lokesh/Documents/Research";
           devices = ["laptop"];
+          ignorePatterns = [".venv/*"];
         };
         "Presentations" = {
           path = "/home/lokesh/Documents/Presentations";
           devices = ["laptop"];
+          ignorePatterns = [".venv/*"];
         };
         "Books" = {
           path = "/home/lokesh/Documents/Books";
           devices = ["laptop"];
-        };
-        "Courses" = {
-          path = "/home/lokesh/Documents/Courses";
-          devices = ["laptop"];
+          ignorePatterns = [".venv/*"];
         };
         "Practice" = {
           path = "/home/lokesh/Desktop/Practice";
           devices = ["laptop"];
+          ignorePatterns = [".venv/*"];
         };
         "Org" = {
           path = "/home/lokesh/Documents/Org";
           devices = ["laptop" "phone"];
+          ignorePatterns = [".venv/*"];
         };
         "Notebook" = {
           path = "/home/lokesh/Documents/Notebook";
           devices = ["laptop" "phone"];
+          ignorePatterns = [".venv/*"];
         };
       };
     };
--- hosts/sudarshan/syncthing.nix
+++ hosts/sudarshan/syncthing.nix
@@ -2,48 +2,55 @@
   services.syncthing = {
     enable = true;
     user = "lokesh";
-    dataDir = "/home/lokesh/.local/share/syncthing";
-    configDir = "/home/lokesh/.config/syncthing";
     openDefaultPorts = true;
     overrideDevices = true;
     overrideFolders = true;
     settings = {
       devices = {
-        "lab" = {id = "BKTCSPE-QCLARTR-5UVN7OB-NRHFAJI-SG36GWR-6NDDDO7-FLQVPV6-MGIU6AT";};
-        "phone" = {id = "KITNSPR-KMBOSUP-RLJS4XX-KILL76P-SR3HAEL-VKQR4U3-JN636OM-FXNZ3AX";};
+        "lab" = {
+          id = "BKTCSPE-QCLARTR-5UVN7OB-NRHFAJI-SG36GWR-6NDDDO7-FLQVPV6-MGIU6AT";
+          autoAcceptFolders = true;
+        };
+        "phone" = {
+          id = "KITNSPR-KMBOSUP-RLJS4XX-KILL76P-SR3HAEL-VKQR4U3-JN636OM-FXNZ3AX";
+          autoAcceptFolders = true;
+        };
       };
       folders = {
         "Projects" = {
           path = "/home/lokesh/Projects";
           devices = ["lab"];
+          ignorePatterns = [".venv/*"];
         };
         "Research" = {
           path = "/home/lokesh/Documents/Research";
           devices = ["lab"];
+          ignorePatterns = [".venv/*"];
         };
         "Presentations" = {
           path = "/home/lokesh/Documents/Presentations";
           devices = ["lab"];
+          ignorePatterns = [".venv/*"];
         };
         "Books" = {
           path = "/home/lokesh/Documents/Books";
           devices = ["lab"];
-        };
-        "Courses" = {
-          path = "/home/lokesh/Documents/Courses";
-          devices = ["lab"];
+          ignorePatterns = [".venv/*"];
         };
         "Practice" = {
           path = "/home/lokesh/Desktop/Practice";
           devices = ["lab"];
+          ignorePatterns = [".venv/*"];
         };
         "Org" = {
           path = "/home/lokesh/Documents/Org";
           devices = ["phone" "lab"];
+          ignorePatterns = [".venv/*"];
         };
         "Notebook" = {
           path = "/home/lokesh/Documents/Notebook";
           devices = ["phone" "lab"];
+          ignorePatterns = [".venv/*"];
         };
         "Personal" = {
           path = "/home/lokesh/Documents/Personal";
