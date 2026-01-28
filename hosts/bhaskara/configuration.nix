a0fc3925d95c4878375fda6ad8b1093ff7858f85
Lokesh Mohanty <lokesh1197@gmail.com>
2026-01-28 14:22:56 +0530

    refactor: move nix-ld from common to hosts

--- hosts/bhaskara/configuration.nix
+++ hosts/bhaskara/configuration.nix
@@ -16,6 +16,12 @@
     nvidiaSettings = true;
   };
   nixpkgs.config.nvidia.acceptLicense = true;
+  gaming.enable = false;
+
+  programs.nix-ld = {
+    enable = true;
+    # libraries = [(pkgs.runCommand "steamrun-lib" {} "mkdir $out; ln -s ${pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")];
+  };
 
   services.cloudflare-warp = {
     enable = true;
@@ -35,9 +41,15 @@
     enable = true;
     nssmdns4 = true;
     openFirewall = true;
+    publish = {
+      enable = true;
+      userServices = true;
+      addresses = true;
+    };
   };
 
   sshServer.enable = true;
+  programs.virt-manager.enable = true;
 
   networking.hostName = "bhaskara";
 }
--- hosts/sudarshan/configuration.nix
+++ hosts/sudarshan/configuration.nix
@@ -8,6 +8,11 @@
   };
   services.xserver.videoDrivers = ["amdgpu"];
 
+  programs.nix-ld = {
+    enable = true;
+    libraries = [(pkgs.runCommand "steamrun-lib" {} "mkdir $out; ln -s ${pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")];
+  };
+
   networking.hostName = "sudarshan";
   gaming.enable = true;
 
